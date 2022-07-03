5a01103c96613ac0779d0734de99aefe45c7e22d
murashev <mikas.murashev@gmail.com>
2022-07-03 22:17:30 +0300

    I added a function

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
-const slides = document.querySelectorAll('.slide')
-for (const slide of slides) {
-    slide.addEventListener('click', ()=> {
-        clearActiveClasses()
-        slide.classList.add('active')
-    })
-}
-function clearActiveClasses() {
-slides.forEach((slide) => {
-    slide.classList.remove('active')
-})
-}
+function slidesPlagin(activeSlide = 4) {
+    const slides = document.querySelectorAll('.slide')
+
+    slides[activeSlide].classList.add('active')
+
+    for (const slide of slides) {
+        slide.addEventListener('click', ()=> {
+            clearActiveClasses()
+
+            slide.classList.add('active')
+        })
+    }
+
+    function clearActiveClasses() {
+        slides.forEach((slide) => {
+            slide.classList.remove('active')
+        })
+    }    
+}       
+
+slidesPlagin ()
